10c9c68ad17b85b43905bd5b4016f4bc71fe9e59
andreztz <andreztz@gmail.com>
2019-08-12 04:19:53 -0300

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ MAINTAINER = "André P. Santos"
 MAINTAINER_EMAIL = "andreztz@gmail.com"
 URL = "https://github.com/andreztz/gitget"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 
 def readme():
@@ -20,6 +20,7 @@ def readme():
 def required():
     with open("requirements.txt") as f:
         return f.read().splitlines()
+
 
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#developer-s-guide
 package = {
@@ -34,8 +35,9 @@ package = {
     "python_requires": REQUIRES_PYTHON,
     "url": URL,
     "license": "my_license",
-    "package_dir": {"": "src"},
-    "packages": find_namespace_packages(where="src"),
+    # "package_dir": {"": "src"},
+    # "packages": find_namespace_packages(where="src"),
+    "packages": find_packages(),
     "install_requires": required(),
     "entry_points": {"console_scripts": ["gitget=src.__main__:main"]},
     "classifiers": [
